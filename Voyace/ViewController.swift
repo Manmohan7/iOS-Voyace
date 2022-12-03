576e5ea909ad9cf0a21dfa8c228306f1a3197b18
Manmohan Singh <er.manmohansingh7@gmail.com>
2022-12-02 19:33:29 -0500

    HomePage with Search and TableView

--- Voyace/ViewController.swift
+++ Voyace/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - Table view data source
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 15
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "table_cell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
 
 }
 
